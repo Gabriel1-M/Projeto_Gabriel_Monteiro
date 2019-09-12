ce9178b3490b77d5c6496e70030f8adc3ed7825a
Gabriel <gabriel.carvalho4@estudante.ufla.br>
2019-09-11 21:47:14 -0300

    Criação da Classe C

---
+++ c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class C {
+    private:
+        string A1;
+        int A2;
+
+    public:
+
+        void CA1() {
+            cout << "CA1";
+        }
+
+        void CA2() {
+            cout << "CA2";
+        }
+};
