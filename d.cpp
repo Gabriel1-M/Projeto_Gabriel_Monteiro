9882a34c88cc9690fbe23e058fcf45edb3358b8c
Gabriel <gabriel.carvalho4@estudante.ufla.br>
2019-09-11 23:14:52 -0300

    Criado nova classe D

---
+++ d.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class D {
+    private:
+        char D1;
+        int D2;
+
+    public:
+
+        void MD1() {
+            cout << "metodo MD1";
+        }
+
+        void MD2() {
+            cout << "metodo MD2";
+        }
+};
